c7350cc0f68d7e6937946ef5fbce0c273bb668ea
Shivam <shivamghuge01@gmail.com>
2024-11-19 18:28:50 +0530

    Update path_innertube.py

--- path_innertube.py
+++ path_innertube.py
@@ -2,7 +2,7 @@ import pytube
 from pytube import innertube
 
 # Save the original constructor of Innertube
-original_init = innertube.Innertube.__init__
+original_init = innertube.InnerTube.__init__
 
 # Define a patched version of the constructor that forces the client to 'WEB'
 def patched_init(self, *args, **kwargs):
